97a2a146ce011e2483c0437a94c0eb601ee1791c
Saskia Mueller <saskiamueller1994@gmail.com>
2020-03-11 09:21:20 +0100

    initial commit

---
+++ B1-build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/smller/Simulationen/B1/exampleB1.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/Users/smller/Simulationen/B1/src/B1ActionInitialization.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/Users/smller/Simulationen/B1/src/B1DetectorConstruction.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/Users/smller/Simulationen/B1/src/B1EventAction.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/Users/smller/Simulationen/B1/src/B1PrimaryGeneratorAction.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/Users/smller/Simulationen/B1/src/B1RunAction.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/Users/smller/Simulationen/B1/src/B1SteppingAction.cc" "/Users/smller/Simulationen/B1-build/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/smller/Simulationen/B1/include"
+  "/Users/smller/geant4.10.06-install/include/Geant4"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include/QtGui"
+  "/usr/local/lib/QtCore.framework/Headers"
+  "/usr/local/etc/qt4/mkspecs/default"
+  "/usr/local/include"
+  "/usr/local/lib/QtCore.framework"
+  "/usr/local/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ B2-build/B2a/CMakeFiles/exampleB2a.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/smller/Simulationen/B2/B2a/exampleB2a.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/exampleB2a.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2ActionInitialization.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2ActionInitialization.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2EventAction.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2EventAction.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2PrimaryGeneratorAction.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2PrimaryGeneratorAction.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2RunAction.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2RunAction.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2TrackerHit.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2TrackerHit.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2TrackerSD.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2TrackerSD.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2aDetectorConstruction.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2aDetectorConstruction.cc.o"
+  "/Users/smller/Simulationen/B2/B2a/src/B2aDetectorMessenger.cc" "/Users/smller/Simulationen/B2-build/B2a/CMakeFiles/exampleB2a.dir/src/B2aDetectorMessenger.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/smller/Simulationen/B2/B2a/include"
+  "/Users/smller/geant4.10.06-install/include/Geant4"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include/QtGui"
+  "/usr/local/lib/QtCore.framework/Headers"
+  "/usr/local/etc/qt4/mkspecs/default"
+  "/usr/local/include"
+  "/usr/local/lib/QtCore.framework"
+  "/usr/local/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
